aacad23c42aded179fd9ea7393373efae05bc4b8
D4sherInc <phil.klotzsche@gmail.com>
2022-12-03 18:20:04 +0100

    more interface logic + documenting

--- src/prolog/tic_tac_toe_with_saving_states.pl
+++ src/prolog/tic_tac_toe_with_saving_states.pl
@@ -18,28 +18,34 @@
 :- dynamic board/1.
 :- dynamic current_player/1.
 :- dynamic legal_actions/1.
+:- dynamic is_terminal/0.
 
 % init(-InitState, -Current_Player, -player0_score)
 % initilization of board, starting player, player0_score
 % reset by retracting (deleting) all  currently existing states
+% TODO: check for different starting players (0;1)
 init(InitState, 0, 0) :-
     reset_game_,
-    board(InitState),
-    assert(current_player(0)).
+    board(InitState).
 
 % apply action to current board
-% save the New Game State, remove the old
-% swap players
+% remove the old GameState, save the New Game State
+% swap players (save them)
 % update legal moves by removing the one used action
+apply_action(GameState, _, "Prolog Custom Error: current GameState not the same as saved!") :-
+    \+ board(GameState), !.
+apply_action(_, Move, "Prolog Custom Error: given Move not legal!") :-
+    legal_actions(Legal_actions),
+    \+ member(Move, Legal_actions), !.
 apply_action(GameState, Move, NewGameState) :-
     legal_actions(Legal_Actions),
     member(Move, Legal_Actions),
-    !,
     board(GameState),
     current_player(Current_Player_ID),
     player_(current_player(Current_Player_ID), Current_Player_Symbol),
     move(GameState, Current_Player_Symbol, Move, NewGameState),
 
+%    update_game_states
     retract(board(GameState)),
     assert(board(NewGameState)),
 
@@ -47,24 +53,30 @@ apply_action(GameState, Move, NewGameState) :-
     other_player(Current_Player_Symbol, Other_Player_Symbol),
     player_(current_player(Other_Player_ID), Other_Player_Symbol),
     assert(current_player(Other_Player_ID)),
+
     update_legal_actions_(NewGameState, Move).
 
 is_terminal(Board) :-
-    Board = [Row1, Row2, Row3],
-    \+ member(0, Row1),
-    \+ member(0, Row2),
-    \+ member(0, Row3), !.
+    flatten(Board, FlattendBoard),
+    findall(I, nth0(I, FlattendBoard, '.'), []),
+    !,
+    assert(is_terminal).
 
 is_terminal(Board) :-
-    wingame(Board, _).
+    wingame(Board, _),
+    assert(is_terminal).
+
+% ------------------------------------------
 
 % internal predicate
 % resets all dynamic predicates
 % sets a new blank board and all actions as legal
 reset_game_ :-
+    % reset dynamic predicates
     retractall(board(_)),
     retractall(current_player(_)),
     retractall(legal_actions(_)),
+    retractall(is_terminal),
 
     assert(board([['.','.','.'],['.','.','.'],['.','.','.']])),
     assert(current_player(0)),
@@ -73,9 +85,6 @@ reset_game_ :-
 % translate PlayerID to symbol to play
 player_(current_player(0), x).
 player_(current_player(1), o).
-
-% ------------------------------------------
-% actual game implementation
 
 % update: if one player won / board is full
 update_legal_actions_(Board, _) :-
@@ -90,6 +99,8 @@ update_legal_actions_(_, Move) :-
     retract(legal_actions(Legal_Actions)),
     assert(legal_actions(NL)).
 
+% ------------------------------------------
+% actual game logic
 
 wingame(Board, Player) :-
     dif(Player, '.'),
