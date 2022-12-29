a49a01e4062fa27242212f901ca6c24b35ee63b9
D4sherInc <phil.klotzsche@gmail.com>
2022-12-29 14:34:04 +0100

    update licencing

---
+++ src/prolog/README.md
@@ -0,0 +1,4 @@
+ This project uses the "connect4.pl" file, which is licensed under the Apache License. This filewas created
+ by Ramon Viñas and Marc Roig and is available on GitHub at
+ https://github.com/rvinas/connect-4-prolog/blob/master/connect4.pl"
+
--- src/prolog/connect4.pl
+++ src/prolog/connect4.pl
@@ -1,3 +1,7 @@
+ % This project uses the connect4.pl file, which is licensed under the Apache License. This filewas created
+ % by Ramon Viñas and Marc Roig and is available on GitHub at
+ % https://github.com/rvinas/connect-4-prolog/blob/master/connect4.pl"
+
 :- module(connect4, []).
 % ------------------------------------------
 % Copyright 2016 Ramon Viñas, Marc Roig
