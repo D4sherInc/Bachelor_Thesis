359e72195f8d7bac19c5e8dd84939a457a20e706
D4sherInc <phil.klotzsche@gmail.com>
2022-12-29 14:13:07 +0100

    refactor: move test cases to files, remove extra files

--- src/prolog/tic_tac_toe_with_saving_states.pl
+++ src/prolog/tic_tac_toe_with_saving_states.pl
@@ -1,16 +1,3 @@
-% all applicable methods:
-%   init (done)
-%   current_player (dynamic:done)
-%   legal_actions (dynamic:done)
-%   apply_action (done)
-%   action_to_string (optional, can by done in python)
-%   is_terminal
-%   returns
-
-
-% tic_tac_toe
-% zero_sum 2 player game
-
 :- use_module(library(clpfd)).
 
 % Interface predicates
@@ -175,3 +162,100 @@ gameinfo(min_utility, -1).
 gameinfo(max_utility, 1).
 gameinfo(utility_sum, 0).
 gameinfo(max_game_length, 9).
+
+% -----------------------------------------------
+%  game logic tests
+% -----------------------------------------------
+:- use_module(library(clpfd)).
+:- use_module(library(plunit)).
+
+:- begin_tests(tic_tac_toe_tests).
+
+test(win1_empty_fail, [fail]) :-
+    Board = [['.','.','.'],
+             ['.','.','.'],
+             ['.','.','.']],
+    wingame(Board, _Player).
+
+test(win2_fully_played, [fail]) :-
+    Board = [o, x, o,
+             x, o, x,
+             x, o, x],
+    wingame(Board, _Player).
+
+test(win3_horizontal, [nondet]) :-
+    Board = [x,x,x,
+             _,_,_,
+             _,_,_],
+    wingame(Board, x).
+
+test(win4_horizontal2, [nondet]) :-
+    Board = [_,_,_,
+             o,o,o,
+             _,_,_],
+    wingame(Board, o).
+
+test(win5_horizontal3, [nondet]) :-
+    Board = [_,_,_,
+             _,_,_,
+             x,x,x],
+    wingame(Board, x).
+
+test(win6_vertical, [nondet]) :-
+    Board = [o,_,_,
+             o,_,_,
+             o,_,_],
+    wingame(Board, o).
+
+test(win7_vertical, [nondet]) :-
+    Board = [_,o,_,
+             _,o,_,
+             _,o,_],
+    wingame(Board, o).
+
+test(win8_vertical, [nondet]) :-
+    Board = [_,_,o,
+             _,_,o,
+             _,_,o],
+    wingame(Board, o).
+
+test(win9_diagonal, [nondet]) :-
+    Board = [x,_,_,
+             _,x,_,
+             _,_,x],
+    wingame(Board, x).
+
+test(win10_diagonal, [nondet]) :-
+    Board = [_,_,x,
+            _,x,_,
+            x,_,_],
+    wingame(Board, x).
+
+test(move1_correct) :-
+    Board = [['.','.','.'],
+             ['.','.','.'],
+             ['.','.','.']],
+    Player = x,
+    Move = 4,
+    move(Board, Player, Move, NewBoard),
+    NewBoard = [['.','.','.'],
+                ['.', x ,'.'],
+                ['.','.','.']].
+
+test(move2_false, [fail]) :-
+    Board = [[ x , o , x ],
+             ['.', x ,'.'],
+             [ o ,'.', o ]],
+    Player = o,
+    Move = 4,
+    move(Board, Player, Move, _NewBoard).
+
+test(move3_false, [fail]) :-
+    Board = [[ x , o , x ],
+             ['.', x ,'.'],
+             [ o ,'.', o ]],
+    Player = x,
+    Move = 4,
+    move(Board, Player, Move, _NewBoard).
+
+:- end_tests(tic_tac_toe_tests).
